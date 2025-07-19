bc8360ff7a05ae10b51b602be2aa1213dc6c927a
Pedro Vinincius <viniciuspedro0905@gmail.com>
2025-07-19 20:38:41 -0300

    Pequenos ajustes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,17 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 
 //Esse arquivo define as rotas do root da aplicação
 export default function RootLayout() {
   return (
-    //Stack faz navegação acumulando as telas, colocando o botão de voltar para voltar nas telas
-    <Stack>
-      {/*O name no Stack.Screen terá o mesmo nome do arquivo do componente/tela ou diretorio da navegação aninhada
+    <>
+      {/*Stack faz navegação acumulando as telas, colocando o botão de voltar para voltar nas telas*/}
+      <Stack>
+        {/*O name no Stack.Screen terá o mesmo nome do arquivo do componente/tela ou diretorio da navegação aninhada
          O headerShown define se vai mostrar a rota na parte de cima da tela*/}
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack>
+      <StatusBar style="light" />
+    </>
   );
 }
